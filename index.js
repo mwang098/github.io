14fc07530f54f9806e9e169879612fe2230df498
EWS Guest EWS Guest <ewsguest233@siebl-0224-14.ews.illinois.edu>
2021-11-13 12:00:09 -0600

    Add index.js file

--- index.js
+++ index.js
@@ -4,4 +4,6 @@ d3.select('h1').style('color', 'lightcoral')
 .text('Updated hello');
 
 d3.select('body').append('p')
-.text('hey');
+.text('hey').style('color', 'lightcyan');
+d3.select('body').append('p')
+.text('favorite food:all food xD').style('color', 'lightblue');
